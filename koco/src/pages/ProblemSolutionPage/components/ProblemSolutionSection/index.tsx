779264efde86e388fcc6beface87b84fb55c647e
hyemimi <hyemi7375@gmail.com>
2025-05-12 17:45:21 +0900

    fix: UI 깨지는 오류 수정

--- koco/src/pages/ProblemSolutionPage/components/ProblemSolutionSection/index.tsx
+++ koco/src/pages/ProblemSolutionPage/components/ProblemSolutionSection/index.tsx
@@ -47,11 +47,17 @@ const ProblemSolutionSection = ({
   return (
     <section className="max-w-2xl mx-auto p-6 bg-surface rounded-lg shadow-md space-y-6 mb-30">
       <h2 className="text-xl font-bold mb-4">| 문제 요약</h2>
-      <p className="whitespace-pre">{formatCode(problemCheck)}</p>
-      <h2 className="text-xl font-bold mb-4">| 알고리즘</h2>
-      <p className="whitespace-pre">{formatCode(algorithm)}</p>
+      <p className="whitespace-pre-line whitespace-pre-line break-words overflow-wrap-anywhere">
+        {formatCode(problemCheck)}
+      </p>
+      <h2 className="text-xl font-bold mb-4 ">| 알고리즘</h2>
+      <p className="whitespace-pre-line whitespace-pre-line break-words overflow-wrap-anywhere">
+        {formatCode(algorithm)}
+      </p>
       <h2 className="text-xl font-bold mb-4">| 풀이</h2>
-      <p className="whitespace-pre">{formatCode(problemSolving)}</p>
+      <p className="whitespace-pre-line whitespace-pre-line break-words overflow-wrap-anywhere">
+        {formatCode(problemSolving)}
+      </p>
       <div>
         <h2 className="text-xl font-bold mb-4">| 솔루션 코드</h2>
         <div className="flex justify-between items-center mb-2">
