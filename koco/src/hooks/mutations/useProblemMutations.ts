8c075da5e29ccfb868b43371e0be16753b69c5ff
hyemimi <hyemi7375@gmail.com>
2025-05-13 10:21:43 +0900

    fix: type error

--- koco/src/hooks/mutations/useProblemMutations.ts
+++ koco/src/hooks/mutations/useProblemMutations.ts
@@ -17,7 +17,7 @@ export const useRegisterSurvey = () => {
 
       const today = new Date().toISOString().split('T')[0];
       queryClient.invalidateQueries({ queryKey: queryKeys.problems.set(today) });
-      queryClient.invalidateQueries({ queryKey: queryKeys.users.dashboard(today) });
+      //queryClient.invalidateQueries({ queryKey: queryKeys.users.dashboard(today) });
     },
   });
 };
