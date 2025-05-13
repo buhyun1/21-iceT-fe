6f5a29badaf7da0b362edc8a8dae64522c008310
buhyun <buhyun8@pukyong.ac.kr>
2025-05-13 16:38:22 +0900

    feat: ga4 사용자 데이터 수집을 위한 기능 추가 (#77)

--- koco/src/App.tsx
+++ koco/src/App.tsx
@@ -1,12 +1,14 @@
 import '@/styles/index.css';
 import RootRoutes from '@/routes/RootRoutes';
 import QueryProvider from './provider/QueryProvider';
+import GoogleAnalytics from '@/utils/GoogleAnalytics';
 
 const App = () => {
   console.log('hellow');
 
   return (
     <QueryProvider>
+      <GoogleAnalytics />
       <RootRoutes />
     </QueryProvider>
   );
