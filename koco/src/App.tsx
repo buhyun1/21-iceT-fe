9d44a6732a9748c75c9adef114c081ca980a9e3c
buhyun <buhyun8@pukyong.ac.kr>
2025-05-13 16:43:49 +0900

    chore: 상위 app에 작성한 ga4 컴포넌트를 하위 route 컴포넌트로 이동(#77)

--- koco/src/App.tsx
+++ koco/src/App.tsx
@@ -1,14 +1,10 @@
 import '@/styles/index.css';
 import RootRoutes from '@/routes/RootRoutes';
 import QueryProvider from './provider/QueryProvider';
-import GoogleAnalytics from '@/utils/GoogleAnalytics';
 
 const App = () => {
-  console.log('hellow');
-
   return (
     <QueryProvider>
-      <GoogleAnalytics />
       <RootRoutes />
     </QueryProvider>
   );
