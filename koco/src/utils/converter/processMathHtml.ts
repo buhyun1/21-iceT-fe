e786340fc7dff5c9adc0c8d80dd5f3df3b0f4778
hyemimi <hyemi7375@gmail.com>
2025-05-21 13:44:52 +0900

    Feat: 문제 영역 html 렌더링 및 수식  데이터 포맷 처리 함수 구현

---
+++ koco/src/utils/converter/processMathHtml.ts
@@ -0,0 +1,19 @@
+/**
+ *
+ * @param html
+ * @returns processedHtml (중복 요소를 가공한 html)
+ */
+export const processMathHtml = (html: string) => {
+  if (!html) return '';
+
+  // LaTeX 수식 추출
+  const processedHtml = html.replace(
+    /<mjx-container[^>]*>[\s\S]*?<span aria-hidden="true" class="no-mathjax mjx-copytext">([\s\S]*?)<\/span><\/mjx-container>/g,
+    (match, latexContent) => {
+      // LaTeX 형식 그대로 유지
+      return latexContent;
+    }
+  );
+
+  return processedHtml;
+};
