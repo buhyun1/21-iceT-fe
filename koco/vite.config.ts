f7c9c2ca5dd598b79cc3bce720ce27abea0bf559
hyemimi <hyemi7375@gmail.com>
2025-05-25 17:56:59 +0900

    Refactor: 빌드 시 바로 시각화 페이지로 이동하는 현상 수정 (환경 변수로 analyze 모드 관리)

--- koco/vite.config.ts
+++ koco/vite.config.ts
@@ -1,12 +1,21 @@
-import { defineConfig } from 'vite';
+import { defineConfig, loadEnv } from 'vite';
 import react from '@vitejs/plugin-react';
 import tsconfigPaths from 'vite-tsconfig-paths';
 import { analyzer } from 'vite-bundle-analyzer';
 import tailwindcss from '@tailwindcss/vite';
 
-export default defineConfig(() => {
+export default defineConfig(({ mode }) => {
+  const plugins = [react(), tsconfigPaths(), tailwindcss()];
+  const env = loadEnv(mode, process.cwd(), '');
+
+  const isAnalyze = env.VITE_IS_ANALYZE === 'true';
+
+  if (isAnalyze) {
+    plugins.push(analyzer());
+  }
+
   return {
-    plugins: [react(), tsconfigPaths(), analyzer(), tailwindcss()],
+    plugins,
     base: '/',
   };
 });
