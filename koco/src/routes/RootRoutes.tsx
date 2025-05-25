7ef647fc7a89d9c8f8bc5091194ec57b15ff8ffe
hyemimi <hyemi7375@gmail.com>
2025-05-25 17:05:13 +0900

    Chore: 설문 페이지, 문제 리스트 조회 페이지, 문제 해설 페이지  lazy loading 적용

--- koco/src/routes/RootRoutes.tsx
+++ koco/src/routes/RootRoutes.tsx
@@ -3,34 +3,40 @@ import AppLayout from '@/components/layout/AppLayout';
 import LoginPage from '@/pages/Login';
 import FirstLoginPage from '@/pages/FirstLoginPage';
 import MainPage from '@/pages/MainPage';
-import SurveyPage from '@/pages/SurveyPage';
-import ProblemListPage from '@/pages/ProblemListPage';
-import ProblemSolutionPage from '@/pages/ProblemSolutionPage';
 import KakaoCallback from '@/pages/KakaoCallbackPage';
 import { ProtectedRoute } from './ProtectedRoute';
 import { AuthProvider } from '@/context/AuthContext';
 import GoogleAnalytics from '@/utils/GoogleAnalytics';
+import React, { Suspense } from 'react';
+import Spinner from '@/components/ui/Spinner';
+
+const SurveyPage = React.lazy(() => import('@/pages/SurveyPage'));
+const ProblemListPage = React.lazy(() => import('@/pages/ProblemListPage'));
+const ProblemSolutionPage = React.lazy(() => import('@/pages/ProblemSolutionPage'));
 
 const RootRoutes = () => {
   return (
     <BrowserRouter basename={'/'}>
       <AuthProvider>
         <GoogleAnalytics />
-        <Routes>
-          <Route element={<AppLayout />}>
-            <Route index element={<LoginPage />} />
-            <Route path="/oauth/kakao/callback" element={<KakaoCallback />} />
+        <Suspense fallback={<Spinner text="로딩중..." />}>
+          <Routes>
+            <Route element={<AppLayout />}>
+              <Route index element={<LoginPage />} />
+              <Route path="/oauth/kakao/callback" element={<KakaoCallback />} />
 
-            {/* 로그인 시 접근 가능한 페이지들 */}
-            <Route element={<ProtectedRoute />}>
-              <Route path="/complete-profile" element={<FirstLoginPage />} />
-              <Route path="/home" element={<MainPage />} />
-              <Route path="/survey" element={<SurveyPage />} />
-              <Route path="/problems" element={<ProblemListPage />} />
-              <Route path="/problems/:id" element={<ProblemSolutionPage />} />
+              {/* 로그인 시 접근 가능한 페이지들 */}
+
+              <Route element={<ProtectedRoute />}>
+                <Route path="/complete-profile" element={<FirstLoginPage />} />
+                <Route path="/home" element={<MainPage />} />
+                <Route path="/survey" element={<SurveyPage />} />
+                <Route path="/problems" element={<ProblemListPage />} />
+                <Route path="/problems/:id" element={<ProblemSolutionPage />} />
+              </Route>
             </Route>
-          </Route>
-        </Routes>
+          </Routes>
+        </Suspense>
       </AuthProvider>
     </BrowserRouter>
   );
