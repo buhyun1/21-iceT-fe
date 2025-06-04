674b3eddec44db8bfa8a0e4e225b1b881c739633
hyemimi <hyemi7375@gmail.com>
2025-06-04 11:03:14 +0900

    Feat: bottom navigation 커뮤니티 이동 버튼 추가, 메뉴 배열로 관리(리팩토링)

--- src/shared/layout/BottomNav.tsx
+++ src/shared/layout/BottomNav.tsx
@@ -2,6 +2,7 @@ import solutionIc from '@/assets/solutionIc.svg';
 import homeIc from '@/assets/homeIc.svg';
 import logoutIc from '@/assets/logoutIc.svg';
 import settingIc from '@/assets/settingIc.png';
+import boardsIc from '@/assets/boardsIc.png';
 import { useNavigate } from 'react-router-dom';
 import { useLogout } from '@/features/auth/hooks/useLogout';
 
@@ -9,53 +10,80 @@ const BottomNav = () => {
   const navigate = useNavigate();
   const { mutate: logoutMutation } = useLogout();
 
+  const menuItem = [
+    {
+      title: '코드 해설집',
+      width: 18,
+      height: 18,
+      icon: solutionIc,
+      alt: 'solution',
+      onClick: () => {
+        navigate('/problems');
+      },
+    },
+    {
+      title: '홈',
+      width: 16,
+      height: 16,
+      icon: homeIc,
+      alt: 'home',
+      onClick: () => {
+        navigate('/home');
+      },
+    },
+    {
+      title: '커뮤니티',
+      width: 18,
+      height: 18,
+      icon: boardsIc,
+      alt: 'boards',
+      onClick: () => {
+        navigate('/boards');
+      },
+    },
+    {
+      title: '로그아웃',
+      width: 16,
+      height: 16,
+      icon: logoutIc,
+      alt: 'logout',
+      onClick: () => {
+        logoutMutation();
+      },
+    },
+    {
+      title: '더보기',
+      width: 16,
+      height: 16,
+      icon: settingIc,
+      alt: 'setting',
+      onClick: () => {
+        navigate('/more');
+      },
+    },
+  ];
+
   return (
     <div className="fixed bottom-0 left-0 w-full flex justify-center z-50 h-20">
       <nav className="w-full max-w-xl bg-white shadow-[0_-2px_4px_rgba(0,0,0,0.06)] flex justify-around items-center py-2 px-6 rounded-t-xl">
-        <div className="group flex flex-col items-center text-xs text-text- gap-1 hover:text-primary-hover transition-colors duration-200">
-          <img
-            onClick={() => navigate('/problems')}
-            src={solutionIc}
-            width={18}
-            height={18}
-            className="group-hover:scale-150 transition-transform duration-200 cursor-pointer"
-            alt="solution"
-          />
-          <p>코드 해설집</p>
-        </div>
-        <div className="group flex flex-col items-center text-xs text-text-primary gap-1 hover:text-primary-hover transition-colors duration-200">
-          <img
-            onClick={() => navigate('/home')}
-            src={homeIc}
-            width={16}
-            height={16}
-            className="group-hover:scale-150 transition-transform duration-200 cursor-pointer"
-            alt="home"
-          />
-          <p>홈</p>
-        </div>
-        <div className="group flex flex-col items-center text-xs text-text-primary gap-1 hover:text-primary-hover transition-colors duration-200">
-          <img
-            onClick={() => logoutMutation()}
-            src={logoutIc}
-            width={16}
-            height={16}
-            className="group-hover:scale-150 transition-transform duration-200 cursor-pointer"
-            alt="Logout icon"
-          />
-          <p>로그아웃</p>
-        </div>
-        <div className="group flex flex-col items-center text-xs text-text-primary gap-1 hover:text-primary-hover transition-colors duration-200">
-          <img
-            onClick={() => navigate('/more')}
-            src={settingIc}
-            width={16}
-            height={16}
-            className="group-hover:scale-150 transition-transform duration-200 cursor-pointer"
-            alt="Logout icon"
-          />
-          <p>더보기</p>
-        </div>
+        {menuItem.map(menu => {
+          return (
+            <div
+              key={menu.alt}
+              className="group flex flex-col items-center text-xs text-text- gap-1 hover:text-primary-hover transition-colors duration-200"
+            >
+              <img
+                onClick={menu.onClick}
+                src={menu.icon}
+                width={menu.width}
+                height={menu.height}
+                className="group-hover:scale-150 transition-transform duration-200 cursor-pointer"
+                alt={menu.alt}
+              />
+              <p>{menu.title}</p>
+            </div>
+          );
+        })}
       </nav>
     </div>
   );
