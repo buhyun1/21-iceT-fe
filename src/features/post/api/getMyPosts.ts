2c60e852a9ee4fab2c822cf96472f4bf4402f835
hyemimi <hyemi7375@gmail.com>
2025-06-12 22:14:42 +0900

    Chore: 내 게시글 리스트 조회 api, 훅 구현

---
+++ src/features/post/api/getMyPosts.ts
@@ -0,0 +1,45 @@
+import { API_SUB_URLS } from '@/constants/apiConfig';
+import axiosInstance from '@/shared/lib/axios';
+import { IApiResponse } from '@/shared/types/ApiResponse';
+
+type Category = {
+  categoryId: number;
+  categoryName: string;
+};
+
+type Author = {
+  userId: number;
+  nickname: string;
+  imgUrl: string;
+};
+
+type Post = {
+  postId: number;
+  problemNumber: number;
+  title: string;
+  createdAt: string; // ISO 문자열 (예: "2025-06-10T00:21:02.664862")
+  categories: Category[];
+  author: Author;
+  commentCount: number;
+  likeCount: number;
+};
+
+export interface IGetMyPostsResponse {
+  nextCursorId: number;
+  hasNext: boolean;
+  posts: Post[];
+}
+
+const getMyPosts = async (cursorId: number | null) => {
+  const params = cursorId !== null ? { cursorId } : {};
+  const response = await axiosInstance.get<IApiResponse<IGetMyPostsResponse>>(
+    `${API_SUB_URLS}/user/myposts`,
+    {
+      params,
+    }
+  );
+
+  return response.data.data;
+};
+
+export default getMyPosts;
