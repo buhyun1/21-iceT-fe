2f3409205d055cc1e462b85448dfd473ab70aa65
hyemimi <hyemi7375@gmail.com>
2025-06-14 20:24:15 +0900

    Feat: 라우팅 연결

---
+++ src/pages/MyPostsPage/index.tsx
@@ -0,0 +1,11 @@
+import PageHeader from '@/shared/layout/PageHeader';
+
+const MyPostPage = () => {
+  return (
+    <div className="bg-background min-h-screen relative">
+      <PageHeader title="나의 게시글" />
+    </div>
+  );
+};
+
+export default MyPostPage;
