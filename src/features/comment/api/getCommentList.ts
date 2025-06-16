eebdc2cbdf66c07d422a3a5e7813a7d19f74887b
hyemimi <hyemi7375@gmail.com>
2025-06-16 15:03:19 +0900

    Chore: comment 타입 파일 분리

--- src/features/comment/api/getCommentList.ts
+++ src/features/comment/api/getCommentList.ts
@@ -1,19 +1,8 @@
 import { API_SUB_URLS_V3 } from '@/constants/apiConfig';
 import axiosInstance from '@/shared/lib/axios';
 import { IApiResponse } from '@/shared/types/ApiResponse';
+import { Comment } from '../types/comment';
 
-type Comment = {
-  id: number;
-  comment: string;
-  author: Author;
-  createdAt: string;
-};
-
-type Author = {
-  userId: number;
-  nickname: string;
-  imgUrl: string;
-};
 interface ICommentListResponse {
   postId: number;
   nextCursorId: number | null;
